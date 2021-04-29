e33c5e6865e08ebd9cef99a5510f1006e2494bb6
romain petit <contact.romain.petit@gmail.com>
2021-04-29 13:29:34 +0200

    Initial Commit

---
+++ LiveStreamIos/LiveStreamIos.h
@@ -0,0 +1,18 @@
+//
+//  LiveStreamIos.h
+//  LiveStreamIos
+//
+//  Created by Romain Petit on 29/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LiveStreamIos.
+FOUNDATION_EXPORT double LiveStreamIosVersionNumber;
+
+//! Project version string for LiveStreamIos.
+FOUNDATION_EXPORT const unsigned char LiveStreamIosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiveStreamIos/PublicHeader.h>
+
+
