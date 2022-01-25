c0b40bd9fd86e6869e57f793ce453b16b9f87d71
romain petit <contact.romain.petit@gmail.com>
2022-01-25 16:43:55 +0100

    sample() fix preview rotation

--- Example/Controllers/ViewController.swift
+++ Example/Controllers/ViewController.swift
@@ -9,9 +9,6 @@ import ApiVideoLiveStream
 class ViewController: UIViewController {
     
     @IBOutlet private weak var preview: UIView!
-    @IBOutlet weak var switchCameraButton: UIButton!
-    @IBOutlet weak var changeAudioButton: UIButton!
-    
     private var liveStream: ApiVideoLiveStream?
     private var errorRtmp: String? = nil
     
@@ -20,6 +17,35 @@ class ViewController: UIViewController {
     private let back = UIImage(systemName: "camera.rotate")
     private let mute = UIImage(systemName: "speaker.slash.circle")
     private let unMute = UIImage(systemName: "speaker.circle")
+    private let parameter = UIImage(systemName: "ellipsis")
+    
+    private var nc = NotificationCenter.default
+    
+    private let muteButton: UIButton = {
+        let muteButton = UIButton()
+        muteButton.setTitleColor(.orange, for: .normal)
+        return muteButton
+    }()
+    
+    private let startButton: UIButton = {
+        let start = UIButton()
+        start.setTitle("Start", for: .normal)
+        start.setTitleColor(.orange, for: .normal)
+        return start
+    }()
+    
+    private let switchButton: UIButton = {
+        let switchBtn = UIButton()
+        switchBtn.setTitleColor(.orange, for: .normal)
+        return switchBtn
+    }()
+    
+    private let parameterButton: UIButton = {
+        let paramBtn = UIButton()
+        paramBtn.setTitleColor(.orange, for: .normal)
+        return paramBtn
+    }()
+    
     
     private func callAlert(code: String?){
         DispatchQueue.main.async {
@@ -52,13 +78,58 @@ class ViewController: UIViewController {
         print("viewDidLoad")
         let audioConfig = AudioConfig(bitrate: 32 * 1000)
         let videoConfig = VideoConfig(bitrate: 2 * 1024 * 1024, resolution: Resolutions.RESOLUTION_720, fps: 30)
-        addAction()
-        self.switchCameraButton.setImage(back, for: .selected)
         do {
             liveStream = try ApiVideoLiveStream(initialAudioConfig: audioConfig, initialVideoConfig: videoConfig, preview: preview)
         } catch {
             print (error)
         }
+        view.addSubview(muteButton)
+        view.addSubview(startButton)
+        view.addSubview(switchButton)
+        view.addSubview(parameterButton)
+        self.muteButton.setImage(unMute, for: .normal)
+        self.switchButton.setImage(back, for: .normal)
+        self.parameterButton.setImage(parameter, for: .normal)
+        
+        self.switchButton.addTarget(self, action: #selector(toggleSwitch), for: .touchUpInside)
+        self.muteButton.addTarget(self, action: #selector(toggleMute), for: .touchUpInside)
+        self.startButton.addTarget(self, action: #selector(toggleLivestream), for: .touchUpInside)
+        self.parameterButton.addTarget(self, action: #selector(navigateToParam), for: .touchUpInside)
+        
+        constraints()
+        
+        addAction()
+        
+        
+    }
+    
+    func constraints(){
+        muteButton.translatesAutoresizingMaskIntoConstraints = false
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        switchButton.translatesAutoresizingMaskIntoConstraints = false
+        parameterButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        muteButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        muteButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        startButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        startButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        switchButton.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        switchButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        parameterButton.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        parameterButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        parameterButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 70).isActive = true
+        parameterButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        
+        startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        startButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
+        
+        muteButton.centerYAnchor.constraint(equalTo: startButton.centerYAnchor).isActive = true
+        switchButton.centerYAnchor.constraint(equalTo: startButton.centerYAnchor).isActive = true
+        
+        muteButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30).isActive = true
+        switchButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30).isActive = true
+        
         
     }
     
@@ -68,11 +139,11 @@ class ViewController: UIViewController {
         liveStream?.videoConfig = SettingsManager.getVideoConfig()
     }
     
-    @IBAction func on(publish: UIButton) {
-        if publish.isSelected {
+    @objc func toggleLivestream(){
+        if startButton.isSelected {
             UIApplication.shared.isIdleTimerDisabled = false
             liveStream?.stopStreaming()
-            publish.setTitle("Start", for: [])
+            startButton.setTitle("Start", for: [])
         } else {
             UIApplication.shared.isIdleTimerDisabled = true
             liveStream?.startStreaming(streamKey: SettingsManager.streamKey, url: SettingsManager.endPoint)
@@ -80,35 +151,50 @@ class ViewController: UIViewController {
                 self.liveStream?.stopStreaming()
                 self.callAlert(code: code)
                 DispatchQueue.main.async {
-                    publish.setTitle("Start", for: [])
-                    publish.isSelected = false
+                    self.startButton.setTitle("Start", for: [])
+                    self.startButton.isSelected = false
                 }
             }
-            publish.setTitle("Stop", for: [])
+            startButton.setTitle("Stop", for: [])
         }
-        publish.isSelected.toggle()
+        startButton.isSelected.toggle()
     }
-    @IBAction func switchCamera(_ sender: Any) {
+    
+    @objc func toggleSwitch(){
         if(liveStream?.camera == .front){
             liveStream?.camera = .back
-            self.switchCameraButton.setImage(self.back, for: .normal)
+            self.switchButton.setImage(self.back, for: .normal)
             
         } else if(liveStream?.camera == .back){
             liveStream?.camera = .front
-            self.switchCameraButton.setImage(self.front, for: .normal)
+            self.switchButton.setImage(self.front, for: .normal)
             
         }
     }
     
-    @IBAction func toggleMute(_ sender: Any) {
+    @objc func toggleMute(){
         if(liveStream!.isMuted){
             liveStream?.isMuted = false
-            self.changeAudioButton.setImage(self.unMute, for: .normal)
+            self.muteButton.setImage(self.unMute, for: .normal)
         }else if(!liveStream!.isMuted){
             liveStream?.isMuted = true
-            self.changeAudioButton.setImage(self.mute, for: .normal)
+            self.muteButton.setImage(self.mute, for: .normal)
         }
     }
+    
+    @objc func navigateToParam(){
+        self.performSegue(withIdentifier: "paramSegue", sender: self)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "paramSegue" {
+            if segue.destination is SettingsViewController {
+
+            }
+        }
+     }
+    
     
 }
 
--- Example/SettingsManager.swift
+++ Example/SettingsManager.swift
@@ -13,8 +13,8 @@ class SettingsManager{
     public static var resolution: Resolutions = Resolutions.RESOLUTION_720
     public static var framerate: Int = 30
     public static var audioBitrate: Int = 32 * 1000
-    public static var endPoint: String = "rtmp://192.168.1.48:1935/s"
-    public static var streamKey: String = "77143677-1898-4fc4-bf21-e5e0f6a7f1de"
+    public static var endPoint: String = "rtmp://broadcast.api.video/s"
+    public static var streamKey: String = ""
     
     
     public static func getVideoConfig() -> VideoConfig{
